99471d522fc12accf052f91977802af728955005
DarwinSanta82 <gerencia@inteligenciadedatos.com>
2025-07-29 18:35:05 -0500

    Incluye 2 funciones nuevas

--- presentacion.py
+++ presentacion.py
@@ -2,3 +2,13 @@ nombre = "Darwin"
 edad = 43
 lenguaje ="Python"
 print(f"Hola, ni nombre es {nombre}, tengo {edad} años y estoy aprendiendo {lenguaje} Hola Estoy modificando")
+
+def comida_favorita():
+    comida = "Pizza"
+    print(f"Mi comida favorita es {comida}")
+
+comida_favorita()
+color = input("¿Cual es tu color favorito? ")
+print(f"Tu color favorito es {color}")
+
+
